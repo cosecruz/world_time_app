d96c8ce557d1b495fa7b17afcdee394ade87f0c4
cosecruz <ebukasamuel205@gmail.com>
2020-11-18 18:00:09 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 
-void main() {
-  runApp(WorldTimeApp());
-}
-
-class WorldTimeApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    ));
---
+++ lib/pages/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Widget build(BuildContext context) {
+    debugShowCheckedModeBanner:
+    false;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location')),
+          ],
+        ),
+      ),
+    );
+  }
+}
