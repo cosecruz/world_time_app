c27c54a9a53bc9f48c4ddfe49611ab9081efc535
cosecruz <ebukasamuel205@gmail.com>
2020-11-18 15:28:08 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WorldTimeApp());
+}
+
+class WorldTimeApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
